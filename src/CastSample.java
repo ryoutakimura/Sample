3f9a57e2fd10348b111212efa0b426dde6f56982
ryoutakimura <yellow11053@gmail.com>
2023-11-11 18:28:23 +0900

    practice

---
+++ src/CastSample.java
@@ -0,0 +1,14 @@
+
+public class CastSample {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 0;
+
+        System.out.println(a / b);
+        System.out.println((double)a / b);
+        System.out.println((double) (a / b));
+
+    }
+
+}
---
+++ src/UseVariable.java
@@ -0,0 +1,15 @@
+
+public class UseVariable {
+
+    public static void main(String[] args) {
+        int number1;
+        String str1;
+        int number2 = 200;
+        String str2 = "xyz";
+        number1 = 1;
+        str1 = "abc";
+        System.out.println(number2);
+        System.out.println(str2);
+    }
+
+}
